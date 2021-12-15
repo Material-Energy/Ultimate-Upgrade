1fcd4396337e14ffa2d56d7c1232b87636a5c9b6
MaterialEnergy <84358558+Material-Energy@users.noreply.github.com>
2021-12-15 16:25:43 -0500

    modified FireWeakness application

--- src/main/java/materialenergy/ultimate/upgrade/effects/FireWeakness.java
+++ src/main/java/materialenergy/ultimate/upgrade/effects/FireWeakness.java
@@ -1,7 +1,6 @@
 package materialenergy.ultimate.upgrade.effects;
 
 import materialenergy.ultimate.upgrade.registry.UUDamageSource;
-import materialenergy.ultimate.upgrade.registry.UUDamageSource;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.effect.StatusEffect;
 import net.minecraft.entity.effect.StatusEffectType;
@@ -12,24 +11,5 @@ import java.util.Objects;
 public class FireWeakness extends StatusEffect {
     public FireWeakness() {
         super(StatusEffectType.HARMFUL, 14981690);
-    }
-
-    @Override
-    public boolean canApplyUpdateEffect(int duration, int amplifier) {
-        // In our case, we just make it return true so that it applies the status effect every tick.
-        return true;
-    }
-
-    // This method is called when it applies the status effect. We implement custom functionality here.
-    @Override
-    public void applyUpdateEffect(LivingEntity entity, int amplifier) {
-        if (entity.getDamageTracker().hasDamage()) {
-            if (Objects.requireNonNull(entity.getDamageTracker().getMostRecentDamage()).getDamageSource().isFire() &&
-                            entity.getDamageTracker().getTimeSinceLastAttack() <= 20) {
-                entity.hurtTime = 0;
-                entity.damage(UUDamageSource.INCINERATE, (float) (amplifier + 1));
-                entity.playSound(SoundEvents.ENTITY_PLAYER_HURT_ON_FIRE, amplifier + 1, 1.0F);
-            }
-        }
     }
 }
